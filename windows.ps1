fb728f3b57056425195a9c04832c5dec2b49714f
dev-a <github@randomail.net>
2017-05-09 10:15:08 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -15,6 +15,7 @@ if(!(Test-Path -Path $foldername )){
 #sinon on le crée
 cd $foldername
 $today = date -format yyyy-MM-dd
+$working_folder = Get-Date –format ‘yyyy-mm-dd_HH-mm-ss’
 if(!(Test-Path -Path $today )){
     New-Item -ItemType directory -Path $today
 }
@@ -23,12 +24,30 @@ if(!(Test-Path -Path $today )){
 #on les compare avec la date du jour-$periode (jours)
 #on supprime ceux qui sont plus vieux que date du jour-$periode
 $table = Get-ChildItem -dir
-foreach($file in $table){
-	$folderdate=([datetime]::ParseExact($file.Name.ToString(),”yyyy-MM-dd”,$null))
-	$suppressdate=Get-Date (get-date).addDays(-$periode)
-	if($folderdate -lt $suppressdate)
-	{
-	 Remove-Item -Recurse -Force $file.Name
+foreach($folder in $table){
+	#on teste si le dossier corresponds à un repertoire de date sinon on le deplace
+	if($folder.Name.ToString() -match "[0-9]{4}\-[0-9]{2}\-[0-9]{2}"){
+		$folderdate=([datetime]::ParseExact($folder.Name.ToString(),”yyyy-MM-dd”,$null))
+		$suppressdate=Get-Date (get-date).addDays(-$periode)
+		if($folderdate -lt $suppressdate)
+		{
+		 Remove-Item -Recurse -Force $folder.Name
+		}
+	}else{
+	if(!(Test-Path -Path $today/$working_folder )){
+		New-Item -ItemType directory -Path $today/$working_folder
+	}
+		
+	
+		Move-Item  -Force $folder.Name $today/$working_folder
+	}
+}
+#on liste les fichiers en vue de les déplacer
+$fileslist = gci -File
+foreach($file in $fileslist){
+	if(!(Test-Path -Path $today/$working_folder )){
+    New-Item -ItemType directory -Path $today/$working_folder
 	}
 
+	Move-Item  -Force $file.Name  $today/$working_folder
 }
